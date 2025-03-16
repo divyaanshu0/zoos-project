031c350ed173e71edb907cabd9e8a21ba0d93758
Divyanshu Kumar <109432531+divyaanshu0@users.noreply.github.com>
2025-03-16 18:09:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, pp. For zoosglobal"
+    return "Hello, pp. For divyanshu forzoosglobal"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

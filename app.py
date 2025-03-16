e449d66983634b760a784b1ff74046f431fa3e38
Divyanshu Kumar <109432531+divyaanshu0@users.noreply.github.com>
2025-03-16 17:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a simple Python app. For zoosglobal"
+    return "Hello, pp. For zoosglobal"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

67301f089e589fc0ba37c1efd0b118e3025f3d42
Divyanshu Kumar <109432531+divyaanshu0@users.noreply.github.com>
2025-03-17 15:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi Everyone .! Welcome to Demo Project. Which is deployed on baremetal kubernetes cluster "
+    return "Hi Team .! Welcome to Demo Project. Which is deployed on baremetal kubernetes cluster "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
